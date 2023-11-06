e407443ecb6d5eae50752ef7b035fdfa45f69bfc
Cedric Stark <cedric.stark@outlook.com>
2023-11-06 20:57:46 +0100

    Add dark theme

--- me_and_you/lib/main.dart
+++ me_and_you/lib/main.dart
@@ -18,7 +18,12 @@ class CanteenApp extends StatelessWidget {
         title: 'Me & You',
         theme: ThemeData(
             useMaterial3: true,
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.blue, brightness: Brightness.light)),
+        darkTheme: ThemeData(
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.blue, brightness: Brightness.dark)),
         home: MenuPage(),
       ),
     );
